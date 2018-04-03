7392a5390ae63d189e3d69df58159b505f005fcc
Ballantyne, Bob <bob.ballantyne@emc.com>
2018-04-02 21:46:23 -0400

    refactored to include php5, supervisord and latest CBF

--- build/action_folders/05.customizations/04.fixupPHP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-[ -d "${SESSIONS_DIR}" ]           || mkdir -p "${SESSIONS_DIR}"
-[ -d "$RUN_DIR" ]                  || mkdir -p "$RUN_DIR"
-
-declare iniFile=/etc/php5/php.ini
-sed -i "s|^.*date.timezone =.*$|date.timezone = ${TZ}|" "$iniFile"
-sed -i "s|^.*session.save_path =.*$|session.save_path = \"${SESSIONS_DIR}\"|" "$iniFile"
